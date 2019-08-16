d237420ec428c957cd628b1627d79481d50a49cd
ming yang shih <39481555+mingyangshih@users.noreply.github.com>
2019-08-16 16:48:16 +0800

    訂單列表

--- src/components/pages/Order.vue
+++ src/components/pages/Order.vue
@@ -13,10 +13,14 @@
       <tbody>
         <tr v-for="(item,index) in orders" :key="item.id">
           <td>{{`${new Date(item.create_at*1000).getFullYear()}/${new Date(item.create_at*1000).getMonth()+1}/${new Date(item.create_at*1000).getDate()}`}}</td>
-          <td>{{users[index].email}}</td>
+          <td>{{email[index]}}</td>
           <td></td>
-          <td></td>
-          <td></td>
+          <!-- <td>{{products[index].id[index]}}</td> -->
+          <td>{{item.total}}</td>
+          <td>
+            <span v-if="item.is_paid" class="text-success font-weight-bold">已付款</span>
+            <span v-else class="text-secondary font-weight-bold">尚未啟用</span>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -27,7 +31,10 @@ export default {
   data() {
     return {
       orders: [],
-      users: []
+      email: [],
+      products: [],
+      id: [],
+      productsDetail: []
     };
   },
   methods: {
@@ -35,12 +42,15 @@ export default {
       const api = `${process.env.APIPATH}/api/${process.env.PATHNAME}/admin/orders?page=${page}`;
       const vm = this;
       this.$http.get(api).then(response => {
-        console.log(response);
+        // console.log(response);
         if (response.data.success) {
           this.orders = response.data.orders;
           response.data.orders.forEach(element => {
-            this.users.push(element.user);
+            this.email.push(element.user.email);
+            this.products.push(element.products);
+            this.id.push(element.id);
           });
+          this.id[4] = 0;
         }
       });
     }
